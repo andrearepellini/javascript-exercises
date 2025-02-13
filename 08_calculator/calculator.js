db39c4e7032702bb786469cb5b87bf33ade19788
repelliniandrea <repellini.andrea@gmail.com>
2025-02-13 17:51:44 +0100

    add solution to exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numsArray) {
+  // 0 is the initial value
+	return numsArray.reduce((acc, num) => acc + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numsArray) {
+  // initial value = 1
+  return numsArray.reduce((acc, num) => acc * num, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exmponent) {
+  let result = 1;
+  for(let i = 1; i <= exmponent; i++) {
+    result *= base;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  for(let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
