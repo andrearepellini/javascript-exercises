127a923d6cc9b37dd7f0d1b98ad63a8d3be381e8
repelliniandrea <repellini.andrea@gmail.com>
2025-02-13 11:50:50 +0100

    add solution to exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    return arr.filter(item => !nums.includes(item));
 };
+
+/*
+solution with a forEach loop
+
+const removeFromArray = function(arr, ...nums) {
+    let newArray = [];
+    arr.forEach(element => {
+        if(!nums.includes(element)) {
+            newArray.push(element);
+        }
+    });
+    return newArray;
+};
+
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
