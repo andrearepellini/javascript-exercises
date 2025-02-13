265df1e441e82882ca9869700903db74ba5657e4
repelliniandrea <repellini.andrea@gmail.com>
2025-02-13 12:09:21 +0100

    add solution to exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if(!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) {
+        return('ERROR');
+    }
+    let sum = 0;
+    while(num1 > num2) {
+        sum += num2;
+        num2++;
+    }
+    while(num2 > num1) {
+        sum += num1;
+        num1++;
+    }
+    sum += num1;
+    return sum;
 };
 
 // Do not edit below this line
