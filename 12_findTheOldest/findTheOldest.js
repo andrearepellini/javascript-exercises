62782c658e3a6f8681401473dcbd1209d17269cd
repelliniandrea <repellini.andrea@gmail.com>
2025-02-15 17:26:09 +0100

    add solution to exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    people.forEach(person => {
+        if(getAge(person) > getAge(oldest)) {
+            oldest = person;
+        }
+    });
 
+    return oldest;
 };
+
+const getAge = function(person) {
+    if(person.yearOfDeath == undefined) {
+        person.yearOfDeath = new Date().getFullYear();
+    }
+    return person.yearOfDeath - person.yearOfBirth;
+}
+
+// solution with reduce
+
+// return people.reduce((oldest, current) => {
+//     return getAge(current) > getAge(oldest) ? current : oldest;
+// });
 
 // Do not edit below this line
 module.exports = findTheOldest;
