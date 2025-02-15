cbb7b675a514a0977bd845b4c796984c5b1161f0
repelliniandrea <repellini.andrea@gmail.com>
2025-02-15 15:51:21 +0100

    add solution to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,33 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    let first = 0;
+    let second = 1;
+    let temp = 0;
+    if (n < 0) {
+        return 'OOPS';
+    }
+    if (n == 1) {
+        return 1;
+    }
 
+    for (let i = 2; i <= n; i++) {
+        temp = first + second;
+        first = second;
+        second = temp;
+    }
+
+    return temp;
 };
+
+// let first = 0;
+//     let second = 1;
+//     let temp = 1;
+//     for(let i = 0; i < n - 1; i++) {
+//         temp = first + second;
+//         first = second;
+//         second = temp;
+//     }
+
+//     return temp;
 
 // Do not edit below this line
 module.exports = fibonacci;
