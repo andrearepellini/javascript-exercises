890796aa802be43052420bdccdf3ec16f5606533
repelliniandrea <repellini.andrea@gmail.com>
2025-02-15 15:08:34 +0100

    add solution to exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+    // two pointers solution
+    inputString = inputString.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let left = 0, right = inputString.length - 1;
 
+    while(left < right) {
+        if(inputString[left] != inputString[right]) return false;
+
+        left++;
+        right--;
+    }
+
+    return true;
 };
+
+// reverse solution
+
+// const palindromes = function (inputString) {
+//     inputString = inputString.toLowerCase().replace(/[^a-z0-9]/g, '');
+//     return inputString === inputString.split('').reverse().join('');
+// };
 
 // Do not edit below this line
 module.exports = palindromes;
