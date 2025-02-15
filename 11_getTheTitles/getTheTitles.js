04b7ce5b533a4c537ecbff0f155aadc6a4e4307d
repelliniandrea <repellini.andrea@gmail.com>
2025-02-15 16:50:05 +0100

    add solution to exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,29 @@
-const getTheTitles = function() {
+const getTheTitles = function (arrayOfBooks) {
+    return arrayOfBooks.filter(book => book.title != undefined)
+        .map(book => book.title);
+}
 
-};
+// using forEach
+
+// const getTheTitles = function (arrayOfBooks) {
+//     let arrayOfTitles = [];
+//     arrayOfBooks.forEach(book => {
+//         if(book.title != undefined) arrayOfTitles.push(element.title);
+//     });
+
+//     return arrayOfTitles;
+// };
+
+// using for .. of ..
+
+// const getTheTitles = function(arrayOfBooks) {
+//     let arrayOfTitles = [];
+//     for(const book of arrayOfBooks) {
+//         if(book.title != undefined) arrayOfTitles.push(book.title);
+//     }
+
+//     return arrayOfTitles;
+// };
 
 // Do not edit below this line
 module.exports = getTheTitles;
